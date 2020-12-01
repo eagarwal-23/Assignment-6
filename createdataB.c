60d2cbc1bf5369b15bf99f3ae38126c8a0948fee
tanvu <tdv@princeton.edu>
2020-12-01 16:37:35 -0500

    Update createdataB.c

--- createdataB.c
+++ createdataB.c
@@ -5,14 +5,14 @@
 int main(void)
 {
     int i;
-    const char name[] = "Tansha Vugarwal";
+    const char* name = "Tansha Vugarwal";
     unsigned long ulReturnAddress = 0x400858;
 
     FILE *psFile;
     
     psFile = fopen("data", "w");
 
-    // Printing out name
+    /* Printing out name */
     fprintf(psFile, "%s", name);
 
     for (i = 0; i < 33; i++)
@@ -23,5 +23,7 @@ int main(void)
     fwrite(&ulReturnAddress, sizeof(unsigned long), 1, psFile);
     
     fclose(psFile);
+
+    return 1;
 }
 
