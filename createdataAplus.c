89485892348bb618ca0dfc0edae2800ce8a6a594
tanvu <tdv@princeton.edu>
2020-12-06 01:13:55 -0500

    Update createdataAplus.c

--- createdataAplus.c
+++ createdataAplus.c
@@ -15,6 +15,13 @@ int main(void)
     /* unsigned int uiBlInstr1 = MiniAssembler_bl(0x400490, 0x420070); */ /* 28-31 */
     /* unsigned int uiBlInstr1 = MiniAssembler_bl(0x400600, 0x420070); */ /* 28-31 */
     
+
+    /* Testing how to use printf */
+    /* Basic idea is we will write NULLBYTE %c NULLBYTE at the end of this file.
+    We will know the place in memory so we can put the address in x0, and then char A in x1,
+    and we can call printf. 
+    Only problem is that printf is segfaulting. */
+    
     /* adrp	x0, 0x400000 */
     unsigned int uiAdrpInstr1 = MiniAssembler_adrp(0, 0x400000, 0x420064); /* 1 */
     /* add	x0, x0, #0x978 */
